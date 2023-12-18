bab7a6acda021eed9b46f3220d1e16c37ca2ff81
Scaha Guerrini <sacha.guerrini@cea.fr>
2023-12-18 14:39:56 +0100

    Small bug fix on xi_sys_psf.py

--- shear/xi_sys/xi_sys_psf.py
+++ shear/xi_sys/xi_sys_psf.py
@@ -1,30 +1,39 @@
 from cosmosis.datablock import names, option_section
 from astropy.io import fits
+import numpy as np
 
+#This file should be added to your cosmosis_standard_library following the path shear/xi_sys/xi_sys_psf.py
 def setup(options):
-    filename = options.get_string('data_file')
+    filename = options.get_string(option_section, 'data_file')
     data = fits.open(filename)
-    return data, filename
+    rho_stats_name = options.get_string(option_section, 'rho_stats_name')
+    xi_plus_name = options.get_string(option_section, 'xi_plus_name')
+    xi_minus_name = options.get_string(option_section, 'xi_minus_name')
+    samples_path = options.get_string(option_section,'samples')
+    return data, filename, rho_stats_name, xi_plus_name, xi_minus_name, samples_path
 
 def execute(block, config):
 
-    data, filename = config
-    cosmo = names.cosmological_parameters
-    xi_sys = names.add_xi_sys
+    data, filename, rho_stats_name, xi_plus_name, xi_minus_name, samples_path = config
 
-    alpha, beta, eta = block[cosmo, 'alpha'], block[cosmo, 'beta'], block[cosmo, 'eta']
+    samples = np.load(samples_path)
+    mean = np.mean(samples, axis=0)
+    cov = np.cov(samples.T)
 
-    xi_p, xi_m = data[block[xi_sys, 'data_sets'][0]], data[block[xi_sys, 'data_sets'][1]]
+    alpha, beta, eta = np.random.multivariate_normal(mean, cov)
+    #alpha, beta, eta = block[cosmo, 'alpha'], block[cosmo, 'beta'], block[cosmo, 'eta']
 
-    rho_stats = data[block[xi_sys, 'rho_stats_name']].data
+    xi_p, xi_m = data[xi_plus_name], data[xi_minus_name]
+
+    rho_stats = data[rho_stats_name].data
 
     xi_sys_p = (
         alpha**2*rho_stats["rho_0_p"]
         + beta**2*rho_stats["rho_1_p"]
         + eta**2*rho_stats["rho_3_p"]
         + 2*alpha*beta*rho_stats["rho_2_p"]
-        + 2*beta*eta*rho_stats["rho_5_p"]
-        + 2*alpha*eta*rho_stats["rho_4_p"]
+        + 2*beta*eta*rho_stats["rho_4_p"]
+        + 2*alpha*eta*rho_stats["rho_5_p"]
     )
 
     xi_sys_m = (
@@ -32,8 +41,8 @@ def execute(block, config):
         + beta**2*rho_stats["rho_1_m"]
         + eta**2*rho_stats["rho_3_m"]
         + 2*alpha*beta*rho_stats["rho_2_m"]
-        + 2*beta*eta*rho_stats["rho_5_m"]
-        + 2*alpha*eta*rho_stats["rho_4_m"]
+        + 2*beta*eta*rho_stats["rho_4_m"]
+        + 2*alpha*eta*rho_stats["rho_5_m"]
     )
 
     xi_p.data["VALUE"] = xi_p.data["RAW_VALUE"]+xi_sys_p #SG: will add xi_sys for each sample. We need to keep in memory the value of the xi_sys
